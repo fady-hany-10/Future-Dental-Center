5af06289205e2b4ecf1c6941c0db31fd07b35cad
fady hany <fadyhanyyy7@gmail.com>
2026-09-11 05:55:01 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,29 +1,29 @@
 const config = {
-    title: "White for woman",
-    subtitle: " Welcome to my page",
-    logo: "logo.png",
+    title: "Future Dental Centre",
+    subtitle: "Dr / mohamed nashat | Dr / Nourhan elemary",
+    logo: "logo (5).png",
     links: [
         {
-            name: "Instagram",
-            url: "https://www.instagram.com/white.for.women?stkn=MTBhenAwZmtvOTR5dg==",
-            icon: "fa-brands fa-instagram",
-            class: "btn-instagram"
-        },
-        {
             name: "Facebook",
-            url: "https://www.facebook.com/share/1CSEAmQhSR/?mibextid=wwXIfr",
+            url: "https://www.facebook.com/share/194B8fnQ19/",
             icon: "fa-brands fa-facebook-f",
             class: "btn-facebook"
         },
         {
+            name: "Instagram",
+            url: "https://www.instagram.com/future_dental_center__?stkn=MXhtYjh5dTc4anM5aQ==",
+            icon: "fa-brands fa-instagram",
+            class: "btn-instagram"
+        },
+        {
             name: "TikTok",
-            url: "https://www.tiktok.com/@white.for.women?_r=1&_t=ZS-99bIeLph8FN",
+            url: "https://www.tiktok.com/@futuredentalcenter?_r=1&_t=ZS-99dApgF2NHu",
             icon: "fa-brands fa-tiktok",
             class: "btn-tiktok"
         },
         {
             name: "InstaPay",
-            url: "https://ipn.eg/S/moazkishk/instapay/3NfHks",
+            url: "https://ipn.eg/S/mohamednashat306/instapay/0vC4Gq",
             icon: "fa-solid fa-wallet",
             class: "btn-instapay"
         }
